bde24136c4c0bb7f3dcf75a4654cd4e79f21b3bd
Marcelo Araújo <marcelo.niklaus@gmail.com>
2021-06-12 00:55:55 -0300

    Exercício classe-05 resolvido

--- classe-05/index.js
+++ classe-05/index.js
@@ -1,3 +1,5 @@
-console.log("Resolvido 2");
+const x1 = 1, x2 = 1, y1 = 1, y2 = 4;
+let d = 0;
+d = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2))
 
-
+console.log(`A distancia entre os pontos (x1,x2) e (y1,y2) é ${d}`);
