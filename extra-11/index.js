d4f56acff9a42add4e8622299ef2adc75c485d7a
Marcelo Araújo <marcelo.niklaus@gmail.com>
2021-06-12 01:12:08 -0300

    Exercício extra-11 resolvido

---
+++ extra-11/index.js
@@ -0,0 +1,4 @@
+const diâmetro = 2;
+let volume = 0, raio = diâmetro / 2;
+volume = (4/3) * Math.PI * Math.pow(raio, 3);
+console.log(`O volume da esfera é de aproximadamente ${volume.toFixed(2)} unidade de medida cúbica`);
