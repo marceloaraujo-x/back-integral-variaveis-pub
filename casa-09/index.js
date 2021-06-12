afd08c19cd00db35bb8b190e4dbf856cd52323c3
Marcelo Araújo <marcelo.niklaus@gmail.com>
2021-06-11 22:10:45 -0300

    Exercício casa-09 resolvido

---
+++ casa-09/index.js
@@ -0,0 +1,5 @@
+let S = 0, n = 6, a = 0;
+S = (n-2)*180;
+a = S / n;
+console.log(`A soma dos ângulos internos desse polígono é ${S} graus`);
+console.log(`Caso os ângulos sejam iguais, medem ${a} graus`);
