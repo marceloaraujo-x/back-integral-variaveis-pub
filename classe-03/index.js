761d9b6f45ed8e3194c27b80c822bcd22d2446e3
Marcelo Araújo <marcelo.niklaus@gmail.com>
2021-06-12 00:27:15 -0300

    Exercício classe-03 resolvido

---
+++ classe-03/index.js
@@ -0,0 +1,4 @@
+let descontoEmPorcentagem = 0, minhaPorcentagem = 0;
+minhaPorcentagem = 80 * 100 / 129.99;
+descontoEmPorcentagem = 100 - minhaPorcentagem;
+console.log(`O cupom de desconto é de ${descontoEmPorcentagem.toFixed(1)} por cento`);
